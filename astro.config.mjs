f610c417ef7f887f134ad1cdc90c36ced231081a
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-01 22:17:12 +0900

    Fix missing dot in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.windhole.net'
+	site: 'https://www.windhole.net', 
 	integrations: [
 		starlight({
 			title: 'windhole's doc site',
