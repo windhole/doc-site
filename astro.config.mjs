07ff2e301fba73ac870e70ccae64b611b58130b7
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-01 22:00:22 +0900

    update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.windhole.net'
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'windhole's doc site',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
