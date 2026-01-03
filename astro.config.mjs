6ef2b3a7c97aff7960aca9f6ac93aadf09418e52
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-04 05:44:26 +0900

    Apply starLightThemeBlack theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,28 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeBlack from 'starlight-theme-black'
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.hotdoc.jp', 
 	integrations: [
 		starlight({
-			title: 'windhole docs',
+			plugins: [
+				starlightThemeBlack({
+					navLinks: [{ //optional
+						label: 'Docs',
+						link: 'getting-started',
+					}],
+					footerText: //optional
+					'Built & designed by [shadcn](https://twitter.com/shadcn). Ported to Astro Starlight by [Adrián UB](https://github.com/adrian-ub). The source code is available on [GitHub](https://github.com/adrian-ub/starlight-theme-black).'
+				})
+			],
+			title: 'windhole docs (under construction)',
 			logo: {
 				src: './public/hotdoc_logo_transp.svg', 
 				replacesTitle: false,
 			},
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Guides',
