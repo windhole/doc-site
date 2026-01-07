4d10c8c37bfa5d8a12518ce8aff38049cc909147
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-07 21:28:32 +0900

    fix build error

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -89,7 +89,7 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+//        {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/facebook/docusaurus',
           label: 'GitHub',
@@ -129,10 +129,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
