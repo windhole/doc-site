0831c3878e1f1abe57b15af7224deb4fb20f08d2
Ko Kazaana <4735171+windhole@users.noreply.github.com>
2026-01-07 21:36:07 +0900

    change log to hotdoc_logo_transp.svg

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/hotdoc_logo_transp.svg',
     colorMode: {
       respectPrefersColorScheme: true,
     },
